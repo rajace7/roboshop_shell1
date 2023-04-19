ef00fc47cde3d18f4dc3ef7fe5a4029b0c64a785
2rpadala <rajvja520002@gmail.com>
2023-04-19 11:46:16 +0530

    payment and dispatch

---
+++ dispatch.sh
@@ -0,0 +1,21 @@
+
+yum install golang -y
+
+useradd roboshop
+
+rm -rf /app
+mkdir /app
+
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+cd /app
+unzip /tmp/dispatch.zip
+
+go mod init dispatch
+go get
+go build
+
+cp /home/centos/roboshop_shell1/dispatch.service /etc/systemd/system/dispatch.service
+
+systemctl daemon-reload
+systemctl enable dispatch
+systemctl restart dispatch
