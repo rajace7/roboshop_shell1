ea2911c337dc2f1b31460e1ea020ab9b0ea33871
2rpadala <rajvja520002@gmail.com>
2023-04-19 09:52:58 +0530

    cart

---
+++ cart.sh
@@ -0,0 +1,21 @@
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+useradd roboshop
+
+rm -rf /app
+mkdir /app
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+cd /app
+unzip /tmp/cart.zip
+
+npm install
+
+cp /home/centos/roboshop_shell1/cart.service /etc/systemd/system/cart.service
+
+systemctl daemon-reload
+systemctl enable cart
+systemctl restart cart
