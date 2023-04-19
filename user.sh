1c536fcc1711e9f01a6b08d8be1411c22cc2d984
2rpadala <rajvja520002@gmail.com>
2023-04-19 20:47:50 +0530

    add user

--- user.sh
+++ user.sh
@@ -1,4 +1,9 @@
-source common.sh
+script_path=$(dirname $0)
+source ${script_path}/common.sh
+
+echo ${script_path}
+
+exit
 
 echo -e "\e[36m>>>>>>>>>>>>>>> CONFIGURE NODE JS REPO >>>>>>>>>>>>\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
