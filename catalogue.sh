43311449adb6e63da90f16735c96eb194df9f6ab
2rpadala <rajvja520002@gmail.com>
2023-04-19 21:26:20 +0530

    script path

--- catalogue.sh
+++ catalogue.sh
@@ -1,6 +1,10 @@
 script_path=$(dirname $0)
 source ${script_path}/common.sh
 
+echo ${script_path}
+
+exit
+
 
 echo -e "\e[36m>>>>>>>>>>>>>>> CONFIGURE NODE JS REPO >>>>>>>>>>>>\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
