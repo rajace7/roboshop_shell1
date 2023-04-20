a9582e9ff64cf498c40324b124f4927d039c6e9f
2rpadala <rajvja520002@gmail.com>
2023-04-20 15:26:11 +0530

    function nodejs

--- catalogue.sh
+++ catalogue.sh
@@ -5,16 +5,6 @@ source ${script_path}/common.sh
 
 component_name=catalogue
 func_nodejs
+schema_setup
 
 
-echo -e "\e[36m>>>>>>>>>>>>>>> COPY REPO FILE TO YUM.REPO.D >>>>>>>>>>>>\e[0m"
-cp ${script_path}/mongod.repo /etc/yum.repos.d/mongod.repo
-
-echo -e "\e[36m>>>>>>>>>>>>>>> INSTALL MONGOD CLIENT >>>>>>>>>>>>\e[0m"
-yum install mongodb-org-shell -y
-
-echo -e "\e[36m>>>>>>>>>>>>>>> LOAD THE SCEHMA >>>>>>>>>>>>\e[0m"
-mongo --host mongodb-dev.rpadaladevops.online </app/schema/catalogue.js
-
-echo -e "\e[36m>>>>>>>>>>>>>>> RESTART THE SERVICE >>>>>>>>>>>>\e[0m"
-systemctl restart catalogue
--- common.sh
+++ common.sh
@@ -7,6 +7,21 @@ print_head()
    echo -e "\e[35m>>>>>>>>>>>>>>> $1 >>>>>>>>>>>>\e[0m"
 }
 
+schema_setup()
+{
+  echo -e "\e[36m>>>>>>>>>>>>>>> COPY REPO FILE TO YUM.REPO.D >>>>>>>>>>>>\e[0m"
+  cp ${script_path}/mongod.repo /etc/yum.repos.d/mongod.repo
+
+  echo -e "\e[36m>>>>>>>>>>>>>>> INSTALL MONGOD CLIENT >>>>>>>>>>>>\e[0m"
+  yum install mongodb-org-shell -y
+
+  echo -e "\e[36m>>>>>>>>>>>>>>> LOAD THE SCEHMA >>>>>>>>>>>>\e[0m"
+  mongo --host mongodb-dev.rpadaladevops.online </app/schema/catalogue.js
+
+  echo -e "\e[36m>>>>>>>>>>>>>>> RESTART THE SERVICE >>>>>>>>>>>>\e[0m"
+  systemctl restart catalogue
+}
+
 func_nodejs(){
 print_head "CONFIGURE NODE JS REPO"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
