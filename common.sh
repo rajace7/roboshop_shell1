86c6413383ace4ecfeb0f27fa813bbb83c95d569
2rpadala <rajvja520002@gmail.com>
2023-04-21 20:18:54 +0530

    exit status

--- common.sh
+++ common.sh
@@ -38,10 +38,10 @@ func_schema_setup()
 func_status_check()
 {
   if [ $? -eq 0 ]; then
-    echo -e "\e[31m SUCCESS\e[0m"
+    echo -e "\e[32m SUCCESS\e[0m"
   else
-    echo -e "\e[32m FAILURE \e[0m"
-    exit
+    echo -e "\e[31m FAILURE \e[0m"
+    exit 1
   fi
 }
 
