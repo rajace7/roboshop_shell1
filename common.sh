680e2466149c7248245bfd85a1c12682b2e6c08d
2rpadala <rajvja520002@gmail.com>
2023-04-21 12:14:12 +0530

    if password

--- common.sh
+++ common.sh
@@ -2,63 +2,103 @@ application_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
-print_head()
+func_print_head()
 {
    echo -e "\e[35m>>>>>>>>>>>>>>> $1 >>>>>>>>>>>>\e[0m"
 }
 
-schema_setup()
+func_schema_setup()
 {
   if [ "$schema_setup" == mongo ]; then
-   print_head "COPY REPO FILE TO YUM.REPO.D"
+   func_print_head "COPY REPO FILE TO YUM.REPO.D"
   cp ${script_path}/mongod.repo /etc/yum.repos.d/mongod.repo
 
-  print_head " INSTALL MONGOD CLIENT "
+  func_print_head " INSTALL MONGOD CLIENT "
   yum install mongodb-org-shell -y
 
- print_head " LOAD THE SCEHMA "
-  mongo --host mongodb-dev.rpadaladevops.online </app/schema/catalogue.js
+ func_print_head " LOAD THE SCEHMA "
+  mongo --host mongodb-dev.rpadaladevops.online </app/schema/${component_name}.js
 
-  print_head" RESTART THE SERVICE "
-  systemctl restart catalogue
+  func_print_head" RESTART THE SERVICE "
+  systemctl restart ${component_name}
   fi
+
+  if [ "$schema_setup" == mysql ]; then
+    func_print_head " INSTALL MYSQL CLIENT "
+    yum install mysql -y
+
+    func_print_head " LOAD THE SCEHMA "
+    mysql -h mysql.rpadaladevops.online -uroot -p${mysql_root_password} < /app/schema/${component_name}.sql
+
+     func_print_head" RESTART THE SERVICE "
+      systemctl restart ${component_name}
+      fi
+
+}
+
+func_app_prereq()
+{
+  func_print_head " ADD APPLICATION USER "
+    useradd ${application_user}
+
+    func_print_head " CREATE APP DIRECTORY "
+    rm -rf /app
+    mkdir /app
+
+    func_print_head " DOWNLOAD THE APPLCATION CONTENT "
+    curl -L -o /tmp/${component_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${component_name}.zip
+    cd /app
+
+    func_print_head " UNZIP APPLICATION CONTENT "
+    unzip /tmp/${component_name}.zip
+}
+
+func_systemd_setup(){
+
+  func_print_head "COPY SERVICE FILE TO SYSTEMD"
+  cp ${script_path}/${component_name}.service /etc/systemd/system/${component_name}.service
+
+  func_print_head "RELOAD THE SERVICE"
+  systemctl daemon-reload
+
+  func_print_head "ENABLE THE SERVICE"
+  systemctl enable ${component_name}
+
+  func_print_head  "RESTART THE SERVICE"
+  systemctl restart ${component_name}
+
 }
 
 func_nodejs(){
-print_head "CONFIGURE NODE JS REPO"
+func_print_head "CONFIGURE NODE JS REPO"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-print_head "INSTALL NODE JS"
+func_print_head "INSTALL NODE JS"
 yum install nodejs -y
 
-print_head "ADD APPLICATION USER"
-useradd ${application_user}
+func_app_prereq
 
-print_head "CREATE APP DIRECTORY"
-rm -rf /app
-mkdir /app
-
-print_head "DOWNLOAD THE APPLCATION CONTENT"
-curl -L -o /tmp/${component_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${component_name}.zip
-cd /app
-
-print_head "UNZIP APPLICATION CONTENT"
-unzip /tmp/${component_name}.zip
-
-print_head "INSTALL THE DEPENDENCIES"
+func_print_head "INSTALL THE DEPENDENCIES"
 npm install
 
-print_head "COPY SERVICE FILE TO SYSTEMD"
-cp ${script_path}/${component_name}.service /etc/systemd/system/${component_name}.service
+func_systemd_setup
 
-print_head "RELOAD THE SERVICE"
-systemctl daemon-reload
 
-print_head "ENABLE THE SERVICE"
-systemctl enable ${component_name}
+func_schema_setup
+}
 
-print_head  "RESTART THE SERVICE"
-systemctl restart ${component_name}
+funct_java()
+{
+  func_print_head " INSTALL MAVEN "
+  yum install maven -y
 
-schema_setup
+  func_app_prereq
+
+  func_print_head " INSTALL THE DEPENDENCIES "
+  mvn clean package
+  mv target/shipping-1.0.jar shipping.jar
+
+  func_systemd_setup
+  func_schema_setup
+
 }
--- shipping.sh
+++ shipping.sh
@@ -3,47 +3,20 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 mysql_root_password=$1
+component_name=shipping
+schema_setup=mysql
+
+
 
 if [ -z "$mysql_root_password" ]; then
   echo mysql root password is missing
   exit
 fi
 
-echo -e "\e[36m>>>>>>>>>>>>>>> INSTALL MAVEN >>>>>>>>>>>>\e[0m"
-yum install maven -y
+component_name=shipping
+schema_setup=mysql
+func_java
 
-echo -e "\e[36m>>>>>>>>>>>>>>> ADD APPLICATION USER >>>>>>>>>>>>\e[0m"
-useradd ${application_user}
 
-echo -e "\e[36m>>>>>>>>>>>>>>> CREATE APP DIRECTORY >>>>>>>>>>>>\e[0m"
-rm -rf /app
-mkdir /app
 
-echo -e "\e[36m>>>>>>>>>>>>>>> DOWNLOAD THE APPLCATION CONTENT >>>>>>>>>>>>\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
 
-echo -e "\e[36m>>>>>>>>>>>>>>> UNZIP APPLICATION CONTENT >>>>>>>>>>>>\e[0m"
-unzip /tmp/shipping.zip
-
-echo -e "\e[36m>>>>>>>>>>>>>>> INSTALL THE DEPENDENCIES >>>>>>>>>>>>\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-echo -e "\e[36m>>>>>>>>>>>>>>> COPY SERVICE FILE TO SYSTEMD >>>>>>>>>>>>\e[0m"
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-
-echo -e "\e[36m>>>>>>>>>>>>>>> RELOAD THE SERVICE >>>>>>>>>>>>\e[0m"
-systemctl daemon-reload
-
-echo -e "\e[36m>>>>>>>>>>>>>>> ENABLE THE SERVICE >>>>>>>>>>>>\e[0m"
-systemctl enable shipping
-
-echo -e "\e[36m>>>>>>>>>>>>>>> RESTART THE SERVICE >>>>>>>>>>>>\e[0m"
-systemctl restart shipping
-
-echo -e "\e[36m>>>>>>>>>>>>>>> INSTALL MYSQL CLIENT >>>>>>>>>>>>\e[0m"
-yum install mysql -y
-
-echo -e "\e[36m>>>>>>>>>>>>>>> LOAD THE SCEHMA >>>>>>>>>>>>\e[0m"
-mysql -h mysql.rpadaladevops.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
