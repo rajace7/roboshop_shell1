d99f91aefb2b06c1c1129b75bff06f112ea5e137
2rpadala <rajvja520002@gmail.com>
2023-04-22 19:46:35 +0530

    if

--- payment.sh
+++ payment.sh
@@ -2,7 +2,12 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
+rabbitmq_appuser_password=$1
 
+if [ -z "$rabbitmq_appuser_password" ]; then
+  echo rabbitmq appuser password is missing
+  exit
+  fi
 
 component_name=payment
 func_python
