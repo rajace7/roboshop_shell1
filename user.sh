480def3daf6072393e8f1e1af762b02e3b32173e
2rpadala <rajvja520002@gmail.com>
2023-04-22 20:40:02 +0530

    removed ip

--- user.sh
+++ user.sh
@@ -7,14 +7,3 @@ schema_setup=mongo
 func_nodejs
 
 
-echo -e "\e[36m>>>>>>>>>>>>>>> COPY REPO FILE TO YUM.REPO.D >>>>>>>>>>>>\e[0m"
-cp ${script_path}/mongod.repo /etc/yum.repos.d/mongod.repo
-
-echo -e "\e[36m>>>>>>>>>>>>>>> INSTALL MONGOD CLIENT >>>>>>>>>>>>\e[0m"
-yum install mongodb-org-shell -y
-
-echo -e "\e[36m>>>>>>>>>>>>>>> LOAD THE SCEHMA >>>>>>>>>>>>\e[0m"
-mongo --host mongodb-dev.rpadaladevops.online </app/schema/user.js
-
-echo -e "\e[36m>>>>>>>>>>>>>>> RESTART THE SERVICE >>>>>>>>>>>>\e[0m"
-systemctl restart user
