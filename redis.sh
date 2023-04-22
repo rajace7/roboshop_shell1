16905846a9fb251f27efdd70da7485816cc94a39
2rpadala <rajvja520002@gmail.com>
2023-04-22 20:07:58 +0530

    removed ip

--- redis.sh
+++ redis.sh
@@ -3,15 +3,15 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 func_print_head " CONFIGURE REDIS  REPO "
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y -y &>>${log_file}
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y  &>>${log_file}
 func_status_check
 
 func_print_head " DISABLE REDIS 8 VERSION "
-dnf module enable redis:remi-6.2 -y -y &>>${log_file}
+dnf module enable redis:remi-6.2 -y  &>>${log_file}
 func_status_check
 
 func_print_head " INSTALL REDIS "
-yum install redis -y -y &>>${log_file}
+yum install redis -y  &>>${log_file}
 func_status_check
 
 func_print_head " CHANGE LOCAL IP ADDRESS TO GLOBAL IP ADDRESS "
@@ -19,11 +19,11 @@ sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf &>>${log_
 func_status_check
 
 func_print_head "ENABLE THE SERVICE "
-systemctl enable redis -y &>>${log_file}
+systemctl enable redis  &>>${log_file}
 func_status_check
 
 func_print_head " RESTART THE SERVICE "
-systemctl restart redis -y &>>${log_file}
+systemctl restart redis  &>>${log_file}
 func_status_check
 
 
