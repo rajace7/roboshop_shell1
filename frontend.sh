bc31bc89dea535823e89bc907cfd26b1876adc45
2rpadala <rajvja520002@gmail.com>
2023-04-24 11:53:27 +0530

    printhead

--- frontend.sh
+++ frontend.sh
@@ -4,36 +4,36 @@ source ${script_path}/common.sh
 
 component_name=frontend
 
-func_printhead "INSTALL NGINX WEBSERVER"
+func_print_head "INSTALL NGINX WEBSERVER"
 yum install nginx -y &>>${log_file}
 func_status_check
 
-func_printhead " ENABLE NGINX WEBSERVER "
+func_print_head " ENABLE NGINX WEBSERVER "
 systemctl enable nginx &>>${log_file}
 func_status_check
 
-func_printhead " RESTART NGINX WEBSERVER "
+func_print_head " RESTART NGINX WEBSERVER "
 systemctl start nginx &>>${log_file}
 func_status_check
 
-func_printhead " REMOVE DEFAULT CONTENT ON WEBSERVER"
+func_print_head " REMOVE DEFAULT CONTENT ON WEBSERVER"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 func_status_check
 
-func_printhead " DOWNLOAD THE APP CONTENT "
+func_print_head " DOWNLOAD THE APP CONTENT "
 curl -o /tmp/${component_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${component_name}.zip &>>${log_file}
 func_status_check
 
 cd /usr/share/nginx/html
 
-func_printhead " UNZIP THE APP CONTENT "
+func_print_head " UNZIP THE APP CONTENT "
 unzip /tmp/${component_name}.zip &>>${log_file}
 func_status_check
 
-func_printhead " COPY REVERSE PROXY FILE TO DEFAULT.D FOLDER m"
+func_print_head " COPY REVERSE PROXY FILE TO DEFAULT.D FOLDER m"
 cp ${script_path}/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 func_status_check
 
-func_printhead "  RESTART NGINX WEBSERVER "
+func_print_head "  RESTART NGINX WEBSERVER "
 systemctl restart nginx &>>${log_file}
 func_status_check
